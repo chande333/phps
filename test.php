1114d1fc1a58777b74181d9aa00eabbe6c6e756a
CRITEOIS\a.benavides <a.benavides@criteo.com>
2022-08-19 13:51:22 +0200

    first commit

---
+++ test.php
@@ -0,0 +1,29 @@
+<?php
+// Allow from any origin
+    if (isset($_SERVER['HTTP_ORIGIN'])){
+        header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+        header('Access-Control-Allow-Credentials: true');
+        header('Access-Control-Max-Age: 86400');
+    }
+
+
+
+function uno(){
+    $temp1 = "Bearer akjdskdksdj";
+    $temp2 = str_replace("Bearer","JWT=",$temp1);
+    echo $temp2;
+    // echo "uno";
+    // call_user_func('tres');
+}
+
+
+function dos(){
+    echo "dos";
+}
+
+
+function tres(){
+    echo "tres";
+}
+
+uno();
