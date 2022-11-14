8a90833444577731da4f62e4a64d0ad2fe91467d
chande333 <benavides.silva@gmail.com>
2022-11-14 11:02:19 +0100

    Update db_sql_ReadWrite.php

--- db_sql_ReadWrite.php
+++ db_sql_ReadWrite.php
@@ -1,10 +1,30 @@
+<?php
+// Allow from any origin
+    if (isset($_SERVER['HTTP_ORIGIN'])){
+        header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+        header('Access-Control-Allow-Credentials: true');
+        header('Access-Control-Max-Age: 86400');
+    }
+
+// Access-Control headers are received during OPTIONS requests
+    if($_SERVER['REQUEST_METHOD'] == 'OPTIONS'){
+        if(isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'])) header("Access-Control-Allow-Methods: GET, POST, 	OPTIONS");         
+        if(isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'])) header("Access-Control-Allow-Headers: {$_SERVER['	HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+        exit(0);
+    }
+
+include "constants.php";
+$call = json_decode(file_get_contents('php://input'));
+
+if ($call){    
+    call_user_func($call->fn);
+}
+
+
 function pingDB(){
-    $call = json_decode(file_get_contents('php://input'));
+    global $call;
 
-    include "constants.php";
-
-
-    $mysqli = new mysqli($servername, $db_username, $db_password, $db_name);
+    $mysqli = new mysqli($GLOBALS['servername'], $GLOBALS['db_username'], $GLOBALS['db_password'], $GLOBALS['db_name']);
 
     /* check connection */
     if ($mysqli->connect_errno) {
@@ -25,12 +45,11 @@ function pingDB(){
 
 
 function readAllDBItems(){
-	$call = json_decode(file_get_contents('php://input'));
-    include "constants.php";
+	global $call;
 
-	$conn = new mysqli($servername, $db_username, $db_password, $db_name);
+	$conn = new mysqli($GLOBALS['servername'], $GLOBALS['db_username'], $GLOBALS['db_password'], $GLOBALS['db_name']);
 
-    $sort = "";
+    	$sort = "";
 
 	// Check connection
 	if ($conn->connect_error) {
