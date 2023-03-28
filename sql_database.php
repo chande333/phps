8e42097654c1de078f97bdc078638614c3f8fa1f
chande333 <benavides.silva@gmail.com>
2023-03-28 20:57:12 +0200

    Update sql_database.php

--- sql_database.php
+++ sql_database.php
@@ -1,50 +1,55 @@
 <?php
 
+$call = json_decode(file_get_contents('php://input')); // id , order, direction
+$servername = "localhost";
+$database  = "";
+$dbusername = "";
+$dbpassword = "";
+
 function readSQLDBfromID(){
 
-	$call = json_decode(file_get_contents('php://input')); // id , order, direction
-	$servername = "localhost";
-	$database  = "";
-	$username = "";
-	$password = "";
+	globals $call;
+	globals $servername;
+	globals $database;
+	globals $dbusername;
+	globals $dbpassword;
+	
+	$outArray = array();
+	$where = "";
+	$order = "";
 
+	if (isset($call->id)){
+	    $where = " WHERE id = '".$call->id."' ";
+	}
 
-    $outArray = array();
-    $where = "";
-    $order = "";
+	if (isset($call->order)){
+	    $order = " ORDER BY ".$call->order." ".$call->direction;
+	}
 
-    if (isset($call->id)){
-        $where = " WHERE id = '".$call->id."' ";
-    }
-
-    if (isset($call->order)){
-        $order = " ORDER BY ".$call->order." ".$call->direction;
-    }
-
-	$conn = new mysqli($servername, $username, $password, $database);
+	$conn = new mysqli($servername, $dbusername, $dbpassword, $database);
 
 	if ($conn->connect_error) {
 	    die("Connection failed: " . $conn->connect_error);
 	}
 	
 	$sql = "SELECT * FROM ".$call->table." ".$where.$order;
-    
+	
 	$result = $conn->query($sql);
 
 	if ($result->num_rows > 0) {
 
-    // output data of each row
-    while($response = $result->fetch_assoc()) {
+	// output data of each row
+	while($response = $result->fetch_assoc()) {
 
-        array_unshift($outArray,$response);
-        
-    }
+	    array_unshift($outArray,$response);
+	    
+	}
 	} else {
-    	echo "0 results";
+		echo "0 results";
 	}
 	
 	$conn->close();
 
 
-    echo json_encode($outArray);
+	echo json_encode($outArray);
 }
