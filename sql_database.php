32c29aa578a592bf56559448f8535ed44ad4e94f
chande333 <benavides.silva@gmail.com>
2023-03-28 21:18:42 +0200

    db vars

--- sql_database.php
+++ sql_database.php
@@ -1,10 +1,27 @@
 <?php
 
-$call = json_decode(file_get_contents('php://input')); // id , order, direction
-$servername = "localhost";
-$dbname  = "";
-$dbusername = "";
-$dbpassword = "";
+$call = json_decode(file_get_contents('php://input')); 
+
+require_once "constants.php";
+//$servername = "localhost";
+//$dbname  = "";
+//$dbusername = "";
+//$dbpassword = "";
+
+
+if (isset($call->fn)){
+    call_user_func($call-> fn);
+}
+else{
+    echo "Error: fn not declared<br>";
+    var_dump($call);
+}
+
+function ping(){
+    echo "pong DB";
+}
+
+
 
 function readSQLDBfromID(){
 
