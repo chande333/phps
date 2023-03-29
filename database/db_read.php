0ff40175d367f209a6d713ef17dfc34a2be0c4c8
CRITEOIS\a.benavides <a.benavides@criteo.com>
2023-03-29 14:03:23 +0200

    adding query and id

--- database/db_read.php
+++ database/db_read.php
@@ -31,6 +31,52 @@ function readSQLDBfromID(){
     $query = "";
     $order = "";
 
+    if (isset($call->id)){
+        $query = " WHERE id = '".$call->id."' ";
+    }
+
+    if (isset($call->order)){
+        $order = " ORDER BY ".urldecode($call->order)." ".urldecode($call->direction);
+    }
+
+	$conn = new mysqli($GLOBALS['servername'], $GLOBALS['$dbusername'], $GLOBALS['$dbpassword'], $GLOBALS['$dbname']);
+
+	if ($conn->connect_error) {
+	    die("Connection failed: " . $conn->connect_error);
+	}
+	
+	$sql = "SELECT * FROM ".$GLOBALS['tableName']." ".$query." ".$order;
+    echo $sql;
+	$result = $conn->query($sql);
+
+	if ($result->num_rows > 0) {
+
+    // output data of each row
+    while($response = $result->fetch_assoc()) {
+
+        array_unshift($outArray,$response);
+        
+    }
+	} else {
+
+    	echo "0 results";
+
+	}
+	
+	$conn->close();
+
+    echo json_encode($outArray);
+}
+
+
+function readSQLDBfromQuery(){
+
+    global $call;
+
+    $outArray = array();
+    $query = "";
+    $order = "";
+
     if (isset($call->query)){
         $query = " WHERE ".urldecode($call->query);
     }
