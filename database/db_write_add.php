545b3a9a9d7df06114a2c6f37de9cd3d49a1481a
chande333 <benavides.silva@gmail.com>
2023-03-29 13:59:10 +0200

    Create db_write_add.php

---
+++ database/db_write_add.php
@@ -0,0 +1,29 @@
+<?php
+
+$call = json_decode(file_get_contents('php://input')); 
+
+require_once "constants.php";
+//$servername = "localhost";
+//$dbname  = "";
+//$dbusername = "";
+//$dbpassword = "";
+
+
+if (isset($call->fn)){
+    call_user_func($call-> fn);
+}
+else{
+    echo "Error: fn not declared<br>";
+    var_dump($call);
+}
+
+function ping(){
+    echo "pong DB";
+}
+
+
+
+function addSQLDBentry(){
+    
+ 
+}
