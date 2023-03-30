19d9e13e878fbe5e16d4d8ae7869dfd8ecedc129
chande333 <benavides.silva@gmail.com>
2023-03-30 22:50:28 +0200

    Update db_read.php

--- database/db_read.php
+++ database/db_read.php
@@ -1,5 +1,8 @@
 <?php
 
+// Two functions 
+// READ from a specific ID and from a given query
+
 function readSQLDBfromID(){
 
     global $globconn;
@@ -16,6 +19,36 @@ function readSQLDBfromID(){
 	if ($result->num_rows > 0) {    
 		while($response = $result->fetch_assoc()) {
 			array_unshift($outArray,$response);        
+		}
+	} else {
+		echo "0 results";
+	}
+	
+	$globconn->close();
+
+	echo json_encode($outArray);
+}
+
+
+
+function readSQLDBfromQuery(){
+
+	global $globconn;
+	$outArray = array();
+
+	$globconn = new mysqli($GLOBALS['servername'], $GLOBALS['dbusername'], $GLOBALS['dbpassword'], $GLOBALS['dbname']);
+
+	if ($globconn->connect_error) {
+		die("Connection failed: " . $globconn->connect_error);
+	}
+	
+	$sql = "SELECT * FROM ".$GLOBALS['tableName']." ".$GLOBALS['query'].$GLOBALS['order'].$GLOBALS['direction'];
+
+	$result = $globconn->query($sql);
+
+	if ($result->num_rows > 0) {
+		while($response = $result->fetch_assoc()) {	
+			array_unshift($outArray,$response);		
 		}
 	} else {
 		echo "0 results";
