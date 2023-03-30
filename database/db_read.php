b2cef91c93a750c72e3a5f17ef068be076a7f363
chande333 <benavides.silva@gmail.com>
2023-03-30 22:39:40 +0200

    Update db_read.php

--- database/db_read.php
+++ database/db_read.php
@@ -1,115 +1,27 @@
 <?php
 
-$call = json_decode(file_get_contents('php://input')); 
-
-require_once "constants.php";
-//$servername = "localhost";
-//$dbname  = "";
-//$dbusername = "";
-//$dbpassword = "";
-
-
-if (isset($call->fn)){
-    call_user_func($call-> fn);
-}
-else{
-    echo "Error: fn not declared<br>";
-    var_dump($call);
-}
-
-function ping(){
-    echo "pong DB";
-}
-
-
-
 function readSQLDBfromID(){
 
-    global $call;
-
+    global $globconn;
     $outArray = array();
-    $query = "";
-    $order = "";
 
-    if (isset($call->id)){
-        $query = " WHERE id = '".$call->id."' ";
-    }
-
-    if (isset($call->order)){
-        $order = " ORDER BY ".urldecode($call->order)." ".urldecode($call->direction);
-    }
-
-	$conn = new mysqli($GLOBALS['servername'], $GLOBALS['$dbusername'], $GLOBALS['$dbpassword'], $GLOBALS['$dbname']);
-
-	if ($conn->connect_error) {
-	    die("Connection failed: " . $conn->connect_error);
+	if ($globconn->connect_error) {
+	    die("Connection failed: " . $globconn->connect_error);
 	}
 	
-	$sql = "SELECT * FROM ".$GLOBALS['tableName']." ".$query." ".$order;
-    echo $sql;
-	$result = $conn->query($sql);
+	$sql = "SELECT * FROM ".$GLOBALS['tableName']." ".$GLOBALS['itemID'].$GLOBALS['order'].$GLOBALS['direction'];
 
-	if ($result->num_rows > 0) {
+	$result = $globconn->query($sql);
 
-    // output data of each row
-    while($response = $result->fetch_assoc()) {
-
-        array_unshift($outArray,$response);
-        
-    }
+	if ($result->num_rows > 0) {    
+		while($response = $result->fetch_assoc()) {
+			array_unshift($outArray,$response);        
+		}
 	} else {
-
-    	echo "0 results";
-
+		echo "0 results";
 	}
 	
-	$conn->close();
+	$globconn->close();
 
-    echo json_encode($outArray);
-}
-
-
-function readSQLDBfromQuery(){
-
-    global $call;
-
-    $outArray = array();
-    $query = "";
-    $order = "";
-
-    if (isset($call->query)){
-        $query = " WHERE ".urldecode($call->query);
-    }
-
-    if (isset($call->order)){
-        $order = " ORDER BY ".urldecode($call->order)." ".urldecode($call->direction);
-    }
-
-	$conn = new mysqli($GLOBALS['servername'], $GLOBALS['$dbusername'], $GLOBALS['$dbpassword'], $GLOBALS['$dbname']);
-
-	if ($conn->connect_error) {
-	    die("Connection failed: " . $conn->connect_error);
-	}
-	
-	$sql = "SELECT * FROM ".$GLOBALS['tableName']." ".$query." ".$order;
-    echo $sql;
-	$result = $conn->query($sql);
-
-	if ($result->num_rows > 0) {
-
-    // output data of each row
-    while($response = $result->fetch_assoc()) {
-
-        array_unshift($outArray,$response);
-        
-    }
-	} else {
-
-    	echo "0 results";
-
-	}
-	
-	$conn->close();
-
-    echo json_encode($outArray);
+	echo json_encode($outArray);
 }
