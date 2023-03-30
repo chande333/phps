f74009529947b8000c8b4c75db546d31c3e3aef5
chande333 <benavides.silva@gmail.com>
2023-03-30 23:22:02 +0200

    $itemID fixed

--- database/db_commons.php
+++ database/db_commons.php
@@ -8,7 +8,7 @@ if ($call->token != ""){ echo "Bad Token";return; }
 $order = isset($call->order) && $call->order != "" ? " ORDER BY ".urldecode($call->order) : "";
 $direction = isset($call->order) && $call->order != "" && isset($call->direction) && $call->direction != "" ? " ".$call->direction : "";
 $query = isset($call->query) ? " WHERE ".urldecode($call->query) : "";
-$itemID =  isset($call->id) ? " WHERE id = ".urldecode($call->id) : "";
+$itemID =  isset($call->id) ? urldecode($call->id) : "";
 
 require_once "constants.php";
 
