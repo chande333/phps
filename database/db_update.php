e83d8e0f96f52756eeeba71c76916cca684bdebb
chande333 <benavides.silva@gmail.com>
2023-03-31 17:26:06 +0200

    Update db_update.php

--- database/db_update.php
+++ database/db_update.php
@@ -1,29 +1,53 @@
 <?php
 
-$call = json_decode(file_get_contents('php://input')); 
-
-require_once "constants.php";
-//$servername = "localhost";
-//$dbname  = "";
-//$dbusername = "";
-//$dbpassword = "";
-
-
-if (isset($call->fn)){
-    call_user_func($call-> fn);
-}
-else{
-    echo "Error: fn not declared<br>";
-    var_dump($call);
-}
-
-function ping(){
-    echo "pong DB";
-}
-
-
+// Considering a table with values
+// date -- it checks if the date is valid
+// 'name','category1', 'category2', 'category3','comment','amount'
 
 function updateSQLDBentry(){
-    
- 
+
+    global $globconn;
+	global $call;
+	$outArray = array();
+
+	if (isset($call->date) && !empty($call->date) && validateDate($call->date)){
+		$date = "date = '".$call->date."' ";
+	} 
+	elseif (empty($call->date)) {
+		$date = "";
+	}
+	else {
+		echo 'Invalid date';
+		return;
+	}
+
+	$fields = array('name','category1', 'category2', 'category3','comment','amount');
+	$fields_sql = array();
+
+	foreach ($fields as $category) {
+		if (isset($call->$category) && !empty($call->$category)) {
+			$fields_sql[] = "{$category} = '" . urldecode($call->$category) . "'";
+		}
+	}
+
+	$fields_sql = implode(', ', $fields_sql);
+
+	if (empty($fields_sql)){
+		echo "Error: Empty values";	
+		return;
+	}
+
+	$globconn = new mysqli($GLOBALS['servername'], $GLOBALS['dbusername'], $GLOBALS['dbpassword'], $GLOBALS['dbname']);
+
+	if ($globconn->connect_error) {
+		die("Connection failed: " . $globconn->connect_error);
+	}
+	
+	$sql = "UPDATE ".$GLOBALS['tableName']." 
+		SET ".$fields_sql."
+		WHERE id = ".$GLOBALS['itemID'];
+	
+	$result = $globconn->query($sql);
+	
+	$globconn->close();
 }
