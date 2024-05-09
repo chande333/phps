10be5d8e4d1253d6ac9f1b25fcbce81d1d157185
chande333 <benavides.silva@gmail.com>
2024-05-09 14:57:46 +0200

    Update db_write_add.php

--- database/db_write_add.php
+++ database/db_write_add.php
@@ -1,21 +1,40 @@
 <?php
-
-function getLastId() {
+function addSQLDBentry(){
+     global $call;
     global $globconn;
-   
+
+    $nextID = getNextId();
+
+    // Check connection
+    if ($globconn->connect_error) {
+        die("Connection failed: " . $globconn->connect_error);
+    }
+
+    $sql = "INSERT INTO #######################
+    (id,name,notes,feed,tags)
+    VALUES (\"" . $nextID . "\",\"" . ($call->notes) . "\",\"" . ($call->feed) . "\",\"" . ($call->tags) . "\")";
+
+    $result = $globconn->query($sql);
+
+    echo $result;
+
+    if ($result == '1') {
+        echo json_encode($call);
+    }
+
+    $globconn->close();   
+ 
+}
+
+function getNextId() {
+    global $globconn;
+
 	$query = "SELECT MAX(id) as lastid
-    FROM ".$GLOBALS['tableName'];
+    FROM ops_knowledge.master_view_favorites";
 	$result = mysqli_query($globconn, $query);
 
-   
     $outArray = [];
     $row = $result->fetch_assoc();
 
-    return ($row['lastid'] + 1);
-}
-
-
-function addSQLDBentry(){
-    
- 
+    echo($row['lastid'] + 1);
 }
